f16eba0dd59555f1085604dc62323b96ed2d20b5
Tomasz Odwald <tomasz.odwald@gmail.com>
2020-10-10 11:32:48 +0200

    Created application logic in main function

--- src/main/java/isa/jjdd/Homeworks.java
+++ src/main/java/isa/jjdd/Homeworks.java
@@ -1,7 +1,23 @@
 package isa.jjdd;
 
+import isa.jjdd.models.LogData;
+import isa.jjdd.provider.LogsProvider;
+import isa.jjdd.repository.LogDataSet;
+
+import java.util.List;
+
 public class Homeworks {
     public static void main(String[] args) {
+        LogsProvider logsProvider = null;
+        String filePath = "";
 
+        List<LogData> logs = logsProvider.loadLogs(filePath);
+        LogDataSet.getInstance().addAll(logs);
+
+        // TODO: generate report for database component
+        // print report for database component
+
+        // TODO: generate report from 18.09
+        // print report from 18.09
     }
 }
