dbe9b9ceb2f9f37e54e0d40c91a7706f4b762579
Tomasz Odwald <tomasz.odwald@gmail.com>
2020-10-10 11:33:08 +0200

    Added list and private constructor for Report class

--- src/main/java/isa/jjdd/models/Report.java
+++ src/main/java/isa/jjdd/models/Report.java
@@ -1,4 +1,11 @@
 package isa.jjdd.models;
 
+import java.util.List;
+
 public class Report {
+    private List<LogData> logData;
+
+    private Report(List<LogData> logData) {
+        this.logData = logData;
+    }
 }
